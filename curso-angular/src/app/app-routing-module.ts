edf253a1af6575dd759640acffd04646a4924335
Diogo Valadão <diogo.svaladao@gmail.com>
2022-06-23 09:55:14 -0300

    exclusão de registro
    finalizado o estudo introdutório

--- curso-angular/src/app/app-routing-module.ts
+++ curso-angular/src/app/app-routing-module.ts
@@ -8,10 +8,12 @@ import { EventosComponent } from "./components/eventos/eventos.component";
 import { IfRenderComponent } from "./components/if-render/if-render.component";
 import { PipesComponent } from "./components/pipes/pipes.component";
 import { TwoWayBindingComponent } from "./components/two-way-binding/two-way-binding.component";
+import { ItemDetailComponent } from "./components/item-detail/item-detail.component";
 
 const routes: Routes = [
     {path: '', component: FirstComponentComponent},
     {path: 'list', component: ListRenderComponent},
+    {path: 'list/:id', component: ItemDetailComponent},
     {path: 'diretivas', component: DirectivasComponent},
     {path: 'emiter', component: EmiterComponent},
     {path: 'eventos', component: EventosComponent},
