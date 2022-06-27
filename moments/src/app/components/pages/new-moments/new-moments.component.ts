dec36ffc966447ae97667f9e9eab0f45673fd066
Diogo Valadão <diogo.svaladao@gmail.com>
2022-06-27 11:35:32 -0300

    formulário criado e estilizado

--- moments/src/app/components/pages/new-moments/new-moments.component.ts
+++ moments/src/app/components/pages/new-moments/new-moments.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-moments.component.css']
 })
 export class NewMomentsComponent implements OnInit {
-
+  btnText = 'Compartilhar!';
   constructor() { }
 
   ngOnInit(): void {
