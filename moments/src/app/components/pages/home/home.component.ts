80ca84293a49f1a2b08f0e169d2224fb032d80d6
Diogo Valadão <diogo.svaladao@gmail.com>
2022-07-04 10:05:07 -0300

    busca finalizada

--- moments/src/app/components/pages/home/home.component.ts
+++ moments/src/app/components/pages/home/home.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { MomentService } from 'src/app/service/moment.service';
 import { Moment } from 'src/app/Moment';
 import { environment } from 'src/environments/environment';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSadCry, faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faAddressBook } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -15,6 +16,9 @@ export class HomeComponent implements OnInit {
   allMoments: Moment[] = []; /* variável para pegar informações na base */
   moments: Moment[] = []; /* variável para filtro que será exibido depois da busca */
   baseApiUrl = environment.baseApiUrl;
+
+  faSearch = faSearch;
+  searchTerm: string = "";
 
   constructor(private momentsService: MomentService) { }
 
@@ -30,5 +34,12 @@ export class HomeComponent implements OnInit {
     });
   }
     
+  /* método para pesquisa na base de dados */
+  search(e: Event): void{
+    const target = e.target as HTMLInputElement; /* passos para pegar informação do HTML */
+    const value = target.value;
+
+    this.moments = this.allMoments.filter(moment => moment.title.toLowerCase().includes(value));
+  }
 
 }
