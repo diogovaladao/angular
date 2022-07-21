3cb0853358e3eae6580329762824db69966e6e61
Diogo Valadão <diogo.svaladao@gmail.com>
2022-07-21 16:05:32 -0300

    salvando produtos na base

--- crud/frontend/src/app/components/product/product-create/product-create.component.ts
+++ crud/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Router } from '@angular/router';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -9,13 +10,21 @@ import { Router } from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: '',
+    price: 0
+  }
+
   constructor(private produdctService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
   }
   
   creatProduct(): void{
-    this.produdctService.showMessage("Produto criado!")
+    this.produdctService.create(this.product).subscribe(() => {
+      this.produdctService.showMessage("Produto criado!")
+      this.router.navigate(["/products"])
+    })
   }
 
   cancel(){
